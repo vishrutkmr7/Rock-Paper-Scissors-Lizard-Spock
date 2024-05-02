1366622bed86c0b328076b5e60be9781eb9a4bcb
Vishrut Jha <vishrutkmr7@icloud.com>
2024-05-01 19:45:23 -0500

    Initial Commit

---
+++ Rock Paper Scissors Lizard Spock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors Lizard Spock
+//
+//  Created by Vishrut Jha on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock Paper Scissors Lizard Spock/Rock_Paper_Scissors_Lizard_SpockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_Scissors_Lizard_SpockApp.swift
+//  Rock Paper Scissors Lizard Spock
+//
+//  Created by Vishrut Jha on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_Scissors_Lizard_SpockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
